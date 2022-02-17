a19dc6aee7c953e7468df71bc9e0d82c63412a5e
unknown <nikhil98patil@gmail.com>
2022-02-18 00:41:33 +0530

    Added linked list use case

---
+++ Runner.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linkedlist;
+
+public class Runner<T> {
+
+	public static void main(String[] args) {
+		LinkedList <Integer> obj = new LinkedList<Integer>();
+		obj.add(56);
+		obj.add(30);
+		obj.add(70);
+		obj.show();
+
+	}
+}
+
+
