755ab4fecf293c8efd2ac8861bc3d19d7b396066
unknown <nikhil98patil@gmail.com>
2022-02-19 09:28:13 +0530

    Added : UC2 Adding New Node To the Next Of Previous Node

---
+++ UC2_AddAtStart/Runner.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linkedlistUC2;
+
+public class Runner<T> {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> obj = new LinkedList<Integer>();
+		obj.add(70);
+		obj.addAtStart(30);
+		obj.addAtStart(56);
+		obj.show();
+	}
+}
+
+
+
